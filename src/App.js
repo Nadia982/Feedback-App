24bdf61a189a6bbbd0f08a7a54b637a0f20759aa
Natalie Gillam <ngillam@hotmail.co.uk>
2024-02-16 21:24:35 +0000

    added FeedbackItem component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import Header from "./components/Header";
+import Header from './components/Header'
+import FeedbackItem from './components/FeedbackItem'
 
-function App() {
-  return (
-    <>
-    <Header/>
-    <div className="container">
-      <h1>My App</h1>
-      
-    </div>
-    </>
-  );
+function App(){
+	return (
+		<>
+			<Header/>
+			<div className='container'>
+				<FeedbackItem/>
+			</div>
+		</>
+)
+
 }
 
-export default App;
+export default App
