5408d5a5e38a5b81a6f190189fabfa9c13c26357
Natalie Gillam <ngillam@hotmail.co.uk>
2024-02-19 23:56:49 +0000

    added close icon with onClick event to feedback cards

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,24 @@
+import {FaTimes} from 'react-icons/fa'
+import PropTypes from 'prop-types'
+import Card from '../components/shared/Card'
+
 function FeedbackItem({item}) {
  //the data is now coming in as a prop, so we don't need useState
-  return (
-    <div className="card">
+  const handleClick = (id)=>{
+    console.log(id)
+  } 
+ return (
+    <Card reverse={false}>
       <div className="num-display">{item.rating}</div>
+      <button onClick={()=>handleClick(item.id)} className="close"><FaTimes color="purple"/></button>
       <div className="text-display">{item.text}</div>
-    </div>
+    </Card>
   );
+}
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+
 }
 
 export default FeedbackItem;
--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -1,18 +1,7 @@
 const FeedbackData = [
-    {id: 1, 
-    rating: 10, 
-text: 'This is a fantastic product', 
-}, 
-{id: 2, 
-    rating: 9, 
-text: 'Superb service', 
-},
-{id: 3, 
-    rating: 8, 
-text: 'Very happy with my purchase.', 
-},
-
-
-]
+  { id: 1, rating: 10, text: "This is a fantastic product" },
+  { id: 2, rating: 9, text: "Superb service" },
+  { id: 3, rating: 8, text: "Very happy with my purchase." },
+];
 
 export default FeedbackData;
