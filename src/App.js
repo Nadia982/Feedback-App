0585f4599ea5fbc53d5b1cd7bb0b0e4a3bf3a413
Natalie Gillam <ngillam@hotmail.co.uk>
2024-03-05 18:58:05 +0000

    added FeedbackForm component for users to provide text feedback

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -16,6 +17,7 @@ function App() {
     <>
       <Header />
       <div className="container">
+      <FeedbackForm/>
         {/* since feedback is our state, whenever that state changes, it'll automatically change within the feedback stats component. */}
         <FeedbackStats feedback={feedback}/>
 
