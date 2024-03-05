6d1da7fbe8a8fb6e32c7f77eef363c710735c35d
Natalie Gillam <ngillam@hotmail.co.uk>
2024-03-05 21:48:37 +0000

    added validation so that 'send' button is disabled until user has typed more than 10 characters

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,10 +1,24 @@
 import { useState } from "react";
 import Card from "./shared/Card";
+import Button from "./shared/Button";
 
 const FeedbackForm = () => {
   const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
   const handleTextChange = (e) => {
+    if(text === ''){
+        setBtnDisabled(true);
+        // message should only show if we have typed something and our text is less than 10 characters long.
+        setMessage(null);
+    } else if (text !== '' && text.trim().length <= 10){
+        setMessage("Please type more than 10 characters");
+        setBtnDisabled(true);
+    } else {
+        setMessage(null);
+        setBtnDisabled(false);
+    }
     setText(e.target.value);
   };
   return (
@@ -14,15 +28,16 @@ const FeedbackForm = () => {
         {/* todo - rating select component */}
         <div className="input-group">
           <input
+        //   className="about-link"
             onChange={handleTextChange}
             type="text"
             placeholder="Write a review"
             value={text}
           />
-          <button type="submit">Send</button>
+          <Button type="submit" isDisabled={btnDisabled} version='secondary'>Send</Button>
         </div>
+        {message && <div className='message'>{message}</div>}
       </form>
-      Form
     </Card>
   );
 };
