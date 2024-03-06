48f94712e80203cace33c628abaf1d3229cef7f6
Natalie Gillam <ngillam@hotmail.co.uk>
2024-03-06 19:53:33 +0000

    added event handler for submitting feedback form

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,44 +1,58 @@
 import { useState } from "react";
-import RatingSelect from './RatingSelect'
+import RatingSelect from "./RatingSelect";
 import Card from "./shared/Card";
 import Button from "./shared/Button";
 
-const FeedbackForm = () => {
+const FeedbackForm = ({handleAdd}) => {
   const [text, setText] = useState("");
   const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState("");
 
   const handleTextChange = (e) => {
-    if(e.target.value === ''){
-        setBtnDisabled(true);
-        // message should only show if we have typed something and our text is less than 10 characters long.
-        setMessage(null);
-    } else if (e.target.value !== '' && e.target.value.trim().length <= 10){
-        setMessage("Please type more than 10 characters");
-        setBtnDisabled(true);
+    if (e.target.value === "") {
+      setBtnDisabled(true);
+      // message should only show if we have typed something and our text is less than 10 characters long.
+      setMessage(null);
+    } else if (e.target.value !== "" && e.target.value.trim().length <= 10) {
+      setMessage("Please type more than 10 characters");
+      setBtnDisabled(true);
     } else {
-        setMessage(null);
-        setBtnDisabled(false);
+      setMessage(null);
+      setBtnDisabled(false);
     }
     setText(e.target.value);
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(text.trim().length > 10){
+        const newFeedback = {
+            text, 
+            rating
+        }
+        handleAdd(newFeedback);
+        setText("");
+    }
+  };
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your experience with us?</h2>
         <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
           <input
-        //   className="about-link"
+            //   className="about-link"
             onChange={handleTextChange}
             type="text"
             placeholder="Write a review"
             value={text}
           />
-          <Button type="submit" isDisabled={btnDisabled} version='secondary'>Send</Button>
+          <Button type="submit" isDisabled={btnDisabled} version="secondary">
+            Send
+          </Button>
         </div>
-        {message && <div className='message'>{message}</div>}
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
